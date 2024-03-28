a49d4a4909346a94e8bfe8ceea60428faff662c6
Krasekkech <kaml-03@mail.ru>
2024-03-28 18:34:40 +0300

    add footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,1 +1,23 @@
 import './footer.sass'
+
+function Footer(){
+    return(
+        <footer>
+            <div className="footer_content">
+                <div className="footer_content_logo">
+                    <ul className="footer_content_logo-item">
+                        <li className="footer_content_logo-item-title">HIP-HOP</li>
+                        <li className="footer_content_logo-item-article">chronicles</li>
+                    </ul>
+                </div>
+                <div className="footer_content_socialmedia">
+
+                </div>
+            </div>
+            <div className="footer_bottom">
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
