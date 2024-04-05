cd8bb3d9ffb5be9aa4eebf9738e9b3e9300086d8
Angelina Litvina <schoolsloth@gmail.com>
2024-04-05 16:26:14 +0300

    header alomost

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,26 @@
-import './index.module.sass'
+import './index.sass'
 
 export const Header = () => {
     return (
         <header className="header">
             <nav className="header__nav">
+                {/*комонент лого, пока просто div logo*/}
+                {/*<Logo/>*/}
+                <div className="logo"></div>
+                <div className="header__nav__btn">
+                    <div className="dropdown">
+                        <div className="dropbtn">Баттлы</div>
+                        <div className="dropdown-content">
+                            <a href="#">Селекты</a>
+                            <a href="#">Баттлы 1х1</a>
+                            <a href="#">Турнирная таблица</a>
+                        </div>
+                    </div>
+                    <div className="header__nav__btn__calendar">
+                        <li className="nav__text">Календарь</li>
+                    </div>
 
+                </div>
             </nav>
         </header>
     );
