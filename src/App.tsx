26f439d1ad2e88c49bd18b88f319af8906061622
Angelina Litvina <schoolsloth@gmail.com>
2024-04-05 14:19:32 +0300

    header update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
 import Index from "./components/Footer";
+import {Header} from './components'
 
 function App() {
 
   return (
     <>
-
-
+        <Header/>
         <Index/>
     </>
   )
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './index.module.sass'
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <nav className="header__nav">
+
+            </nav>
+        </header>
+    );
+};
