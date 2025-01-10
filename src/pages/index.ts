5c6b8fe1f5cb601a0b0dd250ba2ffea05ae60fa9
Angelina Litvina <schoolsloth@gmail.com>
2025-01-10 18:39:32 +0300

    new commit

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
-import {Calendar} from "./Calendar.tsx"
+import {Calendarr} from "./Calendar/Calendar.tsx"
 import {Timer} from "../components/Timer";
 import {HomePage} from "./Homepage/HomePage";
 import {BattleGrid} from "./BattleGrid/BattleGrid";
@@ -6,4 +6,6 @@ import {Login} from "./LoginPage/Login.tsx";
 import {AdminPage} from "./AdminPage.tsx";
 import {Battle1x1} from "./Battle1x1/Battle1x1";
 
+
 export {Calendar, Timer,HomePage, BattleGrid, Login, AdminPage, Battle1x1}
+
