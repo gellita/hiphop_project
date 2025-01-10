feae8b4cfd6be611f1ecbc58ff62f666efd490b5
Angelina Litvina <schoolsloth@gmail.com>
2025-01-10 18:28:30 +0300

    1x1

---
+++ src/pages/Battle1x1/Battle1x1.tsx
@@ -0,0 +1,6 @@
+import React, { useState, useRef } from 'react';
+import styles from './index.module.sass';
+
+export const Battle1x1 = () => {
+
+}
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,5 +4,6 @@ import {HomePage} from "./Homepage/HomePage";
 import {BattleGrid} from "./BattleGrid/BattleGrid";
 import {Login} from "./LoginPage/Login.tsx";
 import {AdminPage} from "./AdminPage.tsx";
+import {Battle1x1} from "./Battle1x1/Battle1x1"
 
-export {Calendar, Timer,HomePage, BattleGrid, Login, AdminPage}
+export {Calendar, Timer,HomePage, BattleGrid, Login, AdminPage, Battle1x1}
