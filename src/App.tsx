e829a0ab72336536003b50c89cc2fe413f8ae185
Angelina Litvina <schoolsloth@gmail.com>
2025-01-19 20:36:08 +0300

    new timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
 import './App.css'
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, useLocation} from 'react-router-dom'
 import {Header, Footer} from './components'
 
 
-import {Timer, HomePage, BattleGrid, Calendar, Login, AdminPage, Battle1x1} from './pages'
+import {Timer, HomePage, BattleGrid, Calendar, Login, AdminPage} from './pages'
 // import {useState} from "react"
 
 
-
 function App() {
-    return (
-        <>
-            {/*<Header reverse={reverse}/>*/}
-            <Header/>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/battles" element={<Battle1x1/>}/>
-                <Route path="/BattleGrid" element={<BattleGrid/>}/>
-                <Route path="/Calendar" element={<Calendar/>}/>
-                <Route path="/admin" element={<AdminPage/>} />
-                <Route path="/login" element={<Login />} />
-            </Routes>
-            <Footer/>
-        </>
+
+  return (
+
+
+      <>
+          {/*<Header reverse={reverse}/>*/}
+          <Header/>
+          <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/battles" element={<Timer/>}/>
+              <Route path="/BattleGrid" element={<BattleGrid/>}/>
+              <Route path="/Calendar" element={<Calendar/>}/>
+              <Route path="/admin" element={<AdminPage/>} />
+              <Route path="/login" element={<Login />} />
+          </Routes>
+          <Footer/>
+      </>
 
 
 
